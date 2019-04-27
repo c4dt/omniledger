b245abc62879339289d9cfcf527cf704a64b4b5b
Linus Gasser <linus.gasser@epfl.ch>
2019-04-27 16:51:15 +0200

    wip

--- src/lib/spec/helpers/reporter.ts
+++ src/lib/spec/helpers/reporter.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import JasmineConsoleReporter from "jasmine-console-reporter";
+import JasmineConsoleReporter from 'jasmine-console-reporter';
 
 // @ts-ignore
 jasmine.getEnv().addReporter(new JasmineConsoleReporter({
