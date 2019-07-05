63cbfdc0f2bc61e58c3dc7434116e2c0f6fd3c7d
tharvik <tharvik@users.noreply.github.com>
2019-07-05 13:06:57 +0200

    dynacred: spawn local srv in test

--- dynacred/spec/data.spec.ts
+++ dynacred/spec/data.spec.ts
@@ -1,5 +1,34 @@
+import * as http from "http";
 import { Data } from "../src/Data";
+import { activateTesting } from "../src/Defaults";
+
+function getLocalServer(): Promise<http.Server> {
+    const srv = http.createServer((req, res) => {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("TODO");
+    });
+
+    // TODO hardcoded addr, better providing it to tests
+    return new Promise((resolve, reject) =>
+        srv.listen(4200, "localhost", () =>
+            resolve(srv),
+        ),
+    );
+}
 
 describe("Data", async () => {
+    let srv: http.Server;
+
+    beforeEach(async () => {
+        srv = await getLocalServer();
+        activateTesting();
+    });
+
+    afterEach(async () => {
+        if (typeof srv !== "undefined") {
+            await new Promise((resolve, reject) => srv.close(resolve));
+        }
+    });
+
     it("can be trivially constructed", async () => new Data());
 });
