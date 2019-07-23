7f8869b133dac24d22da99e603f580bce41ff43b
tharvik <tharvik@users.noreply.github.com>
2019-07-23 17:30:18 +0200

    dynacred: Defaults: rm AdminDarc

--- dynacred/src/Defaults.ts
+++ dynacred/src/Defaults.ts
@@ -6,7 +6,6 @@ import "cross-fetch/polyfill";
 export const Defaults = {
     // IDs
     // TODO remove these when data path is clearer
-    AdminDarc: Buffer.alloc(0), // darc of root user -> ? config.toml
     ByzCoinID: Buffer.alloc(0), // genesis block -> config.toml
 
     // - Testing settings - all settings here are set for the non-testing case. If testing == true, then the
@@ -43,19 +42,16 @@ async function rosterFromAssets(): Promise<Roster> {
 export function activateTesting() {
     Defaults.Testing = true;
     Defaults.ByzCoinID = Buffer.from("5f78d08a260b6fcc0b492448ec272dc4a59794ddf34a9914fdfe4f3faeba616e", "hex");
-    Defaults.AdminDarc = Buffer.from("1cbc6c2c4da749020ffa838e262c952862f582d9730e14c8afe2a1954aa7c50a", "hex");
     Defaults.Ephemeral = Buffer.from("2d9e65673748d99ba5ba7b6be76ff462aaf226461ea226fbb059cbb2af4a7e0c", "hex");
     assetsPath = "/assets/";
 }
 
 export function activateDEDIS() {
     Defaults.ByzCoinID = Buffer.from("9cc36071ccb902a1de7e0d21a2c176d73894b1cf88ae4cc2ba4c95cd76f474f3", "hex");
-    Defaults.AdminDarc = Buffer.from("d427c78474967d6a2ed108713b858c0195cde97f1516f0113fc75b4e9a6dcb52", "hex");
 }
 
 export function activateC4DT() {
     Defaults.ByzCoinID = Buffer.from("5b081e02e38e583085204abfe4553ceb6e0833a530bf8fa476ce2f5c1a9a51ae", "hex");
-    Defaults.AdminDarc = Buffer.from("05e647cbdd220e30e33db4d168bd3ceacd09839ca099ca8ef5ab146c986726f6", "hex");
     Defaults.Ephemeral = Buffer.from("ed3ebf1be6fbe7496a8a31c4124fe560fa2f1651dbfa851c61cbd48c961ab30c", "hex");
 }
 
