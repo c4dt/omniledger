e9230f50edc8480cad87d0a0305a1f493f80bf1a
tharvik <tharvik@users.noreply.github.com>
2019-07-26 11:38:24 +0200

    webapp: trim partner.comp.ts

--- webapp/src/app/c4dt/partner/partner.component.ts
+++ webapp/src/app/c4dt/partner/partner.component.ts
@@ -1,10 +1,7 @@
-import { Location } from "@angular/common";
 import { Component } from "@angular/core";
-import { MatDialog, MatSnackBar } from "@angular/material";
+
 import { showDialogInfo } from "../../../lib/Ui";
 import { ContactsComponent } from "../../admin/contacts/contacts.component";
-import { BcviewerService } from "../../bcviewer/bcviewer.component";
-import { UserData } from "../../user-data.service";
 
 @Component({
     selector: "app-partner",
@@ -12,16 +9,6 @@ import { UserData } from "../../user-data.service";
     templateUrl: "./partner.component.html",
 })
 export class PartnerComponent extends ContactsComponent {
-
-    constructor(dialog: MatDialog,
-                snackBar: MatSnackBar,
-                bcvs: BcviewerService,
-                location: Location,
-                uData: UserData,
-    ) {
-        super(dialog, snackBar, bcvs, location, uData);
-    }
-
     async createSimpleContact() {
         const groups = await this.uData.contact.getGroups();
         if (groups.length === 0) {
