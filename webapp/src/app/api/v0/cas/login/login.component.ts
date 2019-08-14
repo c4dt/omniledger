ac838ffcbba9a8bf638aaa9e02b0714f64dfdc81
tharvik <tharvik@users.noreply.github.com>
2019-08-14 17:15:09 +0200

    webapp/login: gen proof in browser

--- webapp/src/app/api/v0/cas/login/login.component.ts
+++ webapp/src/app/api/v0/cas/login/login.component.ts
@@ -1,11 +1,21 @@
+import assert from "assert";
+import crypto from "crypto";
+import Long from "long";
+
 import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { ActivatedRoute, Router } from "@angular/router";
 
-import { Darc, IdentityWrapper } from "@c4dt/cothority/darc";
+import { Argument, ClientTransaction, InstanceID, Instruction, Proof } from "@c4dt/cothority/byzcoin";
+import CoinInstance from "@c4dt/cothority/byzcoin/contracts/coin-instance";
 
-import { showDialogInfo } from "../../../../../lib/Ui";
 import { UserData } from "../../../../user-data.service";
+
+enum State {
+    LOADING,
+    FAILURE,
+    SUCCESS,
+}
 
 @Component({
     selector: "app-login",
@@ -13,11 +23,25 @@ import { UserData } from "../../../../user-data.service";
     templateUrl: "./login.component.html",
 })
 export class LoginComponent implements OnInit {
-    static readonly casLoginDarc =
-        Buffer.from("b7240d7e4bcc33b4a2af5d2110c512f3118478b964f2b4be9f99b27853702489", "hex");
-    server: string;
-    authorized = false;
-    private service: string;
+
+    // TODO eww, better use Config (as a service maybe?)
+    private static readonly coinInstanceIDForService: Map<string, InstanceID> = new Map([
+        // TODO use correct hex, ie CoinInstance's one
+        ["c4dt.org", ""],
+        ["matrix.c4dt.org", ""],
+    ].map((l) => [l[0], Buffer.from(l[1], "hex")]));
+    private static readonly cost = 1;
+    private static readonly challengeSize = 20;
+    private static readonly challengeHash = "sha256";
+    private static readonly ticketEncoding = "base64";
+    private static readonly txArgName = "challenge";
+
+    state: State;
+    readonly service: string;
+
+    private readonly redirect: URL;
+    private readonly challenge: Buffer;
+    private readonly challengeHashed: Buffer;
 
     constructor(
         private route: ActivatedRoute,
@@ -25,31 +49,85 @@ export class LoginComponent implements OnInit {
         private dialog: MatDialog,
         private uData: UserData,
     ) {
+        const encodedRedirect = this.route.snapshot.queryParamMap.get("service");
+        if (encodedRedirect === null) {
+            throw Error("missing 'service' param");
+        }
+
+        this.redirect = new URL(decodeURI(encodedRedirect));
+        this.service = this.redirect.host;
+        this.state = State.LOADING;
+
+        this.challenge = new Buffer(LoginComponent.challengeSize);
+        crypto.randomFillSync(this.challenge);
+
+        const hash = crypto.createHash(LoginComponent.challengeHash);
+        hash.update(this.challenge);
+        this.challengeHashed = hash.digest();
     }
 
     async ngOnInit() {
-        this.service = decodeURI(this.route.snapshot.queryParamMap.get("service"));
-        this.server = this.service.replace(/^https*:\/\/(.*?)\/.*/, "$1");
-        const auths = await this.uData.bc.checkAuthorization(this.uData.bc.genesisID,
-            LoginComponent.casLoginDarc,
-            IdentityWrapper.fromIdentity(this.uData.keyIdentitySigner));
-        if (auths.indexOf(Darc.ruleSign) >= 0) {
-            this.authorized = true;
-        } else {
-            await showDialogInfo(this.dialog, "No access", "Sorry, you don't have access to " +
-                `${this.service}...`, "Understood");
-        }
+        const success = await this.putChallenge();
+        this.state = success ?  State.SUCCESS : State.FAILURE;
     }
 
     async login() {
-        window.location.href = this.service + "&ticket=ol-" +
-            this.uData.contact.credentialIID.slice(0, 8).toString("hex");
-        window.location.href = this.service + "&ticket=ol-" +
-            this.uData.contact.credentialIID.slice(0, 8).toString("hex");
+        assert.ok(this.state === State.SUCCESS, "asked to login but unable to putChallenge");
+
+        const nextLocation = this.redirect;
+        nextLocation.searchParams.append("ticket",
+            `ST-${this.challenge.toString(LoginComponent.ticketEncoding)}`);
+
+        window.location.replace(nextLocation.href);
     }
 
     async deny() {
         await this.router.navigate(["/"]);
     }
 
+    /**
+     * Try to put the challenge on the chain
+     *
+     * It's encoded as a coin exchange back and forth from the user to the Action's CoinInstance.
+     * The Instruction getting it back contains an Argument with the hash of challenge, so that when the user provides
+     * it, the transaction proves that he was the one generating it.
+     *
+     * @return	success if we managed to put the challenge
+     */
+    private async putChallenge(): Promise<boolean> {
+        const coinInstID = LoginComponent.coinInstanceIDForService.get(this.service);
+        if (coinInstID === undefined) {
+            throw Error("no such service found");
+        }
+
+        const coins = Buffer.from(new Long(LoginComponent.cost).toBytesLE());
+        const createInvoke = (src: InstanceID, args: Argument[]) =>
+            Instruction.createInvoke(
+                src,
+                CoinInstance.contractID,
+                CoinInstance.commandTransfer, args);
+        const transfer = (dst: InstanceID) => [
+            new Argument({name: CoinInstance.argumentCoins, value: coins}),
+            new Argument({name: CoinInstance.argumentDestination, value: dst})];
+
+        const userCoinID = this.uData.coinInstance.id;
+        const ctx = ClientTransaction.make(this.uData.bc.getProtocolVersion(),
+            createInvoke(userCoinID, transfer(coinInstID)),
+            createInvoke(coinInstID, transfer(userCoinID).concat([
+                new Argument({name: LoginComponent.txArgName, value: this.challengeHashed})])));
+        await ctx.updateCountersAndSign(this.uData.bc, [[this.uData.keyIdentitySigner]]);
+
+        try {
+            await this.uData.bc.sendTransactionAndWait(ctx);
+        } catch (err) {
+            if (err instanceof Error && err.message.includes(
+                `Contract coin got Instruction ${ctx.instructions[1].hash().toString("hex")}` +
+                " and returned error: instruction verification failed")) {
+                return false;
+            }
+            throw err;
+        }
+
+        return true;
+    }
 }
