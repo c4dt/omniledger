b4072acd4aaa146998a37c66306ba529eeb74cfd
tharvik <tharvik@users.noreply.github.com>
2019-09-27 11:47:19 +0200

    webapp/login/cas: remove stylesheet file

--- webapp/src/app/api/v0/cas/login/login.component.ts
+++ webapp/src/app/api/v0/cas/login/login.component.ts
@@ -21,7 +21,6 @@ enum State {
 
 @Component({
     selector: "app-login",
-    styleUrls: ["./login.component.css"],
     templateUrl: "./login.component.html",
 })
 export class LoginComponent implements OnInit {
