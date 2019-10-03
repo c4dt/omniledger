607622a4d69595c6e93427d6437d93fabadb1ec9
tharvik <tharvik@users.noreply.github.com>
2019-10-03 13:17:11 +0200

    webapp/admin/contacts: more info for contact

---
+++ webapp/src/app/admin/contacts/contact-info/contact-info.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject, OnInit } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+
+import { Contact } from "@c4dt/dynacred";
+
+import DarcInstance from "@c4dt/cothority/byzcoin/contracts/darc-instance";
+import { IdentityDarc } from "@c4dt/cothority/darc";
+
+@Component({
+    selector: "app-contact-info",
+    styles: [
+        ".waiting { opacity: 0.5; }",
+        ".undefined { opacity: 0.5; }",
+    ],
+    templateUrl: "./contact-info.component.html",
+})
+export class ContactInfoComponent implements OnInit {
+    signerInstanceID: IdentityDarc | undefined;
+    contacts: Contact[] | undefined;
+    actions: DarcInstance[] | undefined;
+    groups: DarcInstance[] | undefined;
+
+    constructor(
+        public dialogRef: MatDialogRef<ContactInfoComponent>,
+        @Inject(MAT_DIALOG_DATA) readonly data: { contact: Contact }) {}
+
+    async ngOnInit() {
+        this.data.contact.getContacts().then(() => this.contacts = this.data.contact.contacts);
+        this.data.contact.getDarcSignIdentity().then((id) => this.signerInstanceID = id);
+        this.data.contact.getActions().then((actions) => this.actions = actions);
+        this.data.contact.getGroups().then((groups) => this.groups = groups);
+    }
+}
