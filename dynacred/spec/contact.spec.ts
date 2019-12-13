8cbd7fbc4552bb7373a678dab0f700867244b955
Linus Gasser <linus.gasser@epfl.ch>
2019-12-13 13:32:20 +0100

    move to crypto-browserify
    replace all references to crypto with crypto-browserify

--- dynacred/spec/contact.spec.ts
+++ dynacred/spec/contact.spec.ts
@@ -1,6 +1,6 @@
 import { Darc, IdentityDarc, IdentityWrapper } from "@c4dt/cothority/darc";
 import Log from "@c4dt/cothority/log";
-import { randomBytes } from "crypto";
+import { randomBytes } from "crypto-browserify";
 import Long from "long";
 import { Data } from "src";
 import { StorageLocalStorage } from "src/Storage";
--- webapp/src/app/api/v0/cas/login/login.component.ts
+++ webapp/src/app/api/v0/cas/login/login.component.ts
@@ -1,4 +1,4 @@
-import crypto from "crypto";
+import crypto from "crypto-browserify";
 import Long from "long";
 import { promisify } from "util";
 
