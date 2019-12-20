a2fec3a44b8f85ef1d99baa49a42aea276e3983e
Linus Gasser <linus.gasser@epfl.ch>
2019-12-20 14:14:07 +0100

    using ObservableArray
    Nativescript viewModels are somewhat difficult to understand. But as long
    as the correct classes are used, everything should be fine.
    
    Also, this fixes a bug in  which made that a  couldn't
    be printed :(

--- mobile/app/pages/lab/group/configure/configure-page.ts
+++ mobile/app/pages/lab/group/configure/configure-page.ts
@@ -1,18 +1,19 @@
 import Log from "@dedis/cothority/log";
-import { fromObject } from "tns-core-modules/data/observable/observable";
+import { ObservableArray } from "tns-core-modules/data/observable-array";
+import { fromObject, fromObjectRecursive } from "tns-core-modules/data/observable/observable";
 import * as dialogs from "tns-core-modules/ui/dialogs";
 import { topmost } from "tns-core-modules/ui/frame/frame";
 import { EventData, Page } from "tns-core-modules/ui/page/page";
 import { isAdmin, uData } from "~/lib/byzcoin-def";
+import { Contact } from "~/lib/dynacred";
 import { GroupContract } from "~/lib/dynacred/group/groupContract";
 import { GroupContractCollection } from "~/lib/dynacred/group/groupContractCollection";
 import { GroupDefinition, IGroupDefinition } from "~/lib/dynacred/group/groupDefinition";
 import { msgFailed } from "~/lib/messages";
-import { Contact } from "~/lib/dynacred";
 
 let page: Page;
 let gcCollection: GroupContractCollection;
-let publicKeyList = [];
+let publicKeyList = new ObservableArray<Contact>();
 let selectedPublicKeys = [];
 
 // tslint:disable: object-literal-sort-keys
@@ -29,7 +30,7 @@ const dataFormDetails = fromObject({
     signoffs: "",
 });
 
-const viewModel = fromObject({
+const viewModel = fromObjectRecursive({
     dataForm,
     dataFormDetails,
     isAdmin,
@@ -69,7 +70,8 @@ export async function navigatingTo(args: EventData) {
             dataFormDetails.set("id", page.navigationContext.id);
         }
     }
-    publicKeyList = [uData.contact];
+    publicKeyList.splice(0);
+    publicKeyList.push(uData.contact);
     selectedPublicKeys = [uData.keyIdentity._public];
     viewModel.set("publicKeyList", publicKeyList);
     page.bindingContext = viewModel;
@@ -93,8 +95,8 @@ export async function propose() {
     let groupDefinition = new GroupDefinition(variables);
     let contract: GroupContract;
     if (!gcCollection) {
-        console.log("new", groupDefinition.purpose);
-        console.log("voteThreshold", groupDefinition.voteThreshold);
+        Log.print("new", groupDefinition.purpose);
+        Log.print("voteThreshold", groupDefinition.voteThreshold);
         gcCollection = new GroupContractCollection(variables.purpose);
         // genesis group contract: c0
         contract = gcCollection.createGroupContract(undefined, groupDefinition);
@@ -103,7 +105,7 @@ export async function propose() {
         contract = gcCollection.createGroupContract(contract, groupDefinition);
         gcCollection.sign(contract, uData.keyIdentity._private);
     } else {
-        console.log("gcCollection", gcCollection);
+        Log.print("gcCollection", gcCollection);
         if (groupDefinition.isSimilarTo(gcCollection.getCurrentGroupContract().groupDefinition)) {
             dialogs.alert({
                 title: "Warning",
@@ -132,32 +134,31 @@ export async function addPublicKey(args: any) {
             cancelButtonText: "Cancel",
             actions: uData.contacts.map((c) => c.alias),
         });
-        console.log("3");
-        console.log("length", uData.contacts.length);
-        // console.log("gris", await uData.contacts[0].getDevices());
+        Log.print("3");
+        Log.print("length", uData.contacts.length);
+        // Log.print("gris", await uData.contacts[0].getDevices());
         // const devices = await uData.contact[0].getDevices();
-        // console.log("a");
+        // Log.print("a");
         // const pubKeys = devices.map((dev) => dev.pubKey);
-        // console.log("a");
+        // Log.print("a");
         // Log.print(pubKeys[0].point);
-        console.log("4");
+        Log.print("4");
         const contact = uData.contacts.find((c) => {
-            console.log(c.alias);
+            Log.print(c.alias);
             return c.alias === result;
         });
-        console.log("5", contact !== null);
+        Log.print("5", contact !== null);
         if (contact != null) {
             const devices = await contact.getDevices();
-            console.log("devices");
+            Log.print("devices:", devices);
             const publicKeys = devices.map((d) => d.pubKey);
             // TODO if there is multiple public key need to choose!
             publicKeyList.push(contact as Contact);
-            console.log("publicKeyList", publicKeyList);
-            viewModel.set("publicKeyList", publicKeyList);
+            Log.print("publicKeyList", publicKeyList);
         }
     } catch (e) {
-        console.log("error");
-        msgFailed(e.toString(), "Error");
+        Log.catch("error");
+        return msgFailed(e.toString(), "Error");
     }
 }
 
