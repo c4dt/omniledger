08c407aaede0cd561fcf83970913bed519a71668
tharvik <tharvik@users.noreply.github.com>
2020-04-23 15:53:30 +0200

    webapp/cas/stress: oneshot run

--- webapp/cas/cmd/cas-stress/main.go
+++ webapp/cas/cmd/cas-stress/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"os"
-	"time"
 
 	"github.com/sclevine/agouti"
 	"github.com/urfave/cli"
@@ -16,64 +15,68 @@ const omniledgerURL = "https://demo.c4dt.org/omniledger"
 func main() {
 	app := cli.NewApp()
 
-	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "with-wordpress-url"},
-		cli.StringFlag{Name: "with-matrix-url"},
-		cli.StringFlag{
-			Name:     "user-data-path",
-			Required: true,
+	app.Flags = []cli.Flag{cli.StringFlag{
+		Name:     "user-data-path",
+		Required: true,
+	}}
+
+	app.Commands = []cli.Command{
+		{
+			Name: "wordpress",
+			Action: func(c *cli.Context) error {
+				return runClicker(c, clickWordpress)
+			},
+		}, {
+			Name: "matrix",
+			Action: func(c *cli.Context) error {
+				return runClicker(c, clickMatrix)
+			},
 		},
-		cli.DurationFlag{
-			Name:  "new-login-delay",
-			Value: time.Minute,
-		},
-	}
-	app.Action = func(c *cli.Context) error {
-		userData, err := getUserData(c.String("user-data-path"))
-		if err != nil {
-			return err
-		}
-
-		driver := agouti.ChromeDriver(agouti.ChromeOptions("args", []string{"headless"}))
-		if err := driver.Start(); err != nil {
-			return err
-		}
-
-		clickers := make([]func(*agouti.Page) error, 0)
-		if url := c.String("with-matrix-url"); url != "" {
-			clickers = append(clickers, func(p *agouti.Page) error { return clickMatrix(url, p) })
-		}
-		if url := c.String("with-wordpress-url"); url != "" {
-			clickers = append(clickers, func(p *agouti.Page) error { return clickWordpress(url, p) })
-		}
-
-		ticker := time.Tick(c.Duration("new-login-delay"))
-		for {
-			page, err := getNewPage(driver, userData)
-			if err != nil {
-				return err
-			}
-
-			i := rand.Intn(len(clickers))
-			clicker := clickers[i]
-
-			err = clicker(page)
-			if err != nil {
-				return err
-			}
-
-			if err := page.Destroy(); err != nil {
-				return err
-			}
-
-			<-ticker
-		}
 	}
 
 	err := app.Run(os.Args)
 	if err != nil {
-		panic(err)
+		fmt.Println("error:", err)
+		os.Exit(1)
 	}
+}
+
+func runClicker(c *cli.Context, clicker func(string, *agouti.Page) error) error {
+	userData, err := getUserData(c.GlobalString("user-data-path"))
+	if err != nil {
+		return err
+	}
+
+	if c.NArg() != 1 {
+		return errors.New("no URL given")
+	}
+	url := c.Args().First()
+
+	driver := agouti.ChromeDriver(agouti.ChromeOptions("args", []string{"headless"}))
+	if err := driver.Start(); err != nil {
+		return err
+	}
+
+	page, err := driver.NewPage()
+	if err != nil {
+		return err
+	}
+	// how much time until failing to find smth
+	page.SetImplicitWait(60 * 1000)
+
+	if err := injectUser(page, userData); err != nil {
+		return err
+	}
+
+	if err := clicker(url, page); err != nil {
+		return err
+	}
+
+	if err := page.Destroy(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func getUserData(path string) (string, error) {
@@ -91,22 +94,26 @@ func injectUser(page *agouti.Page, userData string) error {
 	}
 
 	storageDBInjector := `
-	var request = window.indexedDB.open("dynasent", 10);
-	request.onerror = console.log;
-	request.onupgradeneeded = function(event) {
-		event.target.result.
-			createObjectStore("contacts", {keyPath: "key"}).
-			put(JSON.parse(data));
-	};
+	const dynasentDone = new Promise((resolve) =>
+		window.indexedDB.open("dynasent", 10).onupgradeneeded = (event) =>
+			event.target.result.
+				createObjectStore("contacts", {keyPath: "key"}).
+				put(JSON.parse(data)).onsuccess = resolve)
 
-	var request = window.indexedDB.open("__dbnames", 10);
-	request.onerror = console.log;
-	request.onupgradeneeded = function(event) {
-		const store = event.target.result.
-			createObjectStore("dbnames", {keyPath: "name"});
-		store.createIndex("buffer", "buffer", {unique: false, multiEntry: false});
-		store.put({name: "dynasent"});
-	};`
+	const dbnamesDone = new Promise((resolve) =>
+		window.indexedDB.open("__dbnames", 10).onupgradeneeded = (event) => {
+			const store = event.target.result.
+				createObjectStore("dbnames", {keyPath: "name"})
+			store.createIndex("buffer", "buffer", {unique: false, multiEntry: false})
+			store.put({name: "dynasent"}).onsuccess = resolve
+		})
+
+	Promise.all([dynasentDone, dbnamesDone]).then(() => {
+		const elem = document.createElement("span")
+		elem.id = "run-script-done"
+		document.body.appendChild(elem)
+	})
+	`
 
 	if err := page.RunScript(
 		storageDBInjector,
@@ -115,8 +122,7 @@ func injectUser(page *agouti.Page, userData string) error {
 		return err
 	}
 
-	// TODO eww, how to wait for javascript async then?
-	time.Sleep(10 * time.Second)
+	page.FindByID("run-script-done")
 
 	return nil
 }
@@ -128,10 +134,13 @@ func clickMatrix(matrixURL string, page *agouti.Page) error {
 	if err := page.FindByLink("Sign In").Click(); err != nil {
 		return err
 	}
-	if err := page.FindByLink("Sign in with single sign-on").Click(); err != nil {
+	if err := page.FindByClass("mx_Login_sso_link").Click(); err != nil {
 		return err
 	}
 	if err := page.FindByButton("Login").Click(); err != nil {
+		return err
+	}
+	if err := page.FindByLink("I trust this address").Click(); err != nil {
 		return err
 	}
 
@@ -150,7 +159,10 @@ func clickWordpress(wordpressURL string, page *agouti.Page) error {
 	if err := page.Navigate(wordpressURL); err != nil {
 		return err
 	}
-	if err := page.FindByLink("Members only").Click(); err != nil {
+	if err := page.FindByLink("About us").Click(); err != nil {
+		return err
+	}
+	if err := page.FindByLink("Partner Login").Click(); err != nil {
 		return err
 	}
 	if err := page.FindByLink("Sign in with OmniLedger").Click(); err != nil {
@@ -169,21 +181,4 @@ func clickWordpress(wordpressURL string, page *agouti.Page) error {
 	}
 
 	return nil
-}
-
-func getNewPage(driver *agouti.WebDriver, userData string) (*agouti.Page, error) {
-	page, err := driver.NewPage()
-	if err != nil {
-		return nil, err
-	}
-
-	// how much time until failing to find smth
-	page.SetImplicitWait(60 * 1000)
-
-	err = injectUser(page, userData)
-	if err != nil {
-		return nil, err
-	}
-
-	return page, nil
 }
