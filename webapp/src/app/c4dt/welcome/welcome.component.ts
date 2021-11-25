6385b93ca9e3005c1112b19e95e9cec3fec18184
Linus Gasser <linus.gasser@epfl.ch>
2021-11-25 11:39:28 +0100

    Only show available services
    In the welcome screen, only display the services available to this user.

--- webapp/src/app/c4dt/welcome/welcome.component.ts
+++ webapp/src/app/c4dt/welcome/welcome.component.ts
@@ -1,17 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { UserService } from "src/app/user.service";
+import { ByzCoinService } from "src/app/byz-coin.service";
+import { Darc, IdentityWrapper } from "@dedis/cothority/darc";
+import { Action, ConfigService } from "src/app/api/v0/cas/config.service";
 
 @Component({
     selector: "app-welcome",
     templateUrl: "./welcome.component.html",
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
     name: string;
+    matrixAccess = false;
+    c4dtAccess = false;
 
-    constructor(private user: UserService) {
+    constructor(private user: UserService,
+                private builder: ByzCoinService,
+                private configService: ConfigService,
+    ) {
         user.credStructBS.credPublic.alias.subscribe((alias) => {
             this.name = alias;
         });
+    }
+
+    async ngOnInit(): Promise<void> {
+        const config = await this.configService.config;
+        this.c4dtAccess = await this.checkService(config.serviceToAction.get("https://www.c4dt.org/"));
+        this.matrixAccess = await this.checkService(config.serviceToAction.get("https://matrix.c4dt.org/"));
+    }
+
+    private async checkService(action: Action | undefined): Promise<boolean> {
+        if (action !== undefined) {
+            const rules = await this.builder.bc.checkAuthorization(
+                this.builder.bc.genesisID, action.darc,
+                IdentityWrapper.fromIdentity(this.user.kiSigner));
+            return rules.indexOf(Darc.ruleSign) !== -1;
+        }
+        return false;
     }
 }
