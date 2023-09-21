c6d925827e42110b2aef13a686cbd814ed63ea7c
Linus Gasser <linus.gasser@epfl.ch>
2023-09-21 11:29:37 +0200

    Make re-using signup link nicer

--- webapp/src/app/register/device/device.component.ts
+++ webapp/src/app/register/device/device.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
-import { MatSnackBar } from "@angular/material/snack-bar";
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import URL from 'url-parse';
 
-import Log from "@dedis/cothority/log";
+import Log from '@dedis/cothority/log';
 
-import { Router } from "@angular/router";
-import { ByzCoinService } from "src/app/byz-coin.service";
-import { showDialogOKC, showTransactions, TProgress } from "src/lib/Ui";
+import { Router } from '@angular/router';
+import { ByzCoinService } from 'src/app/byz-coin.service';
+import { showDialogInfo, showDialogOKC, showTransactions, TProgress } from 'src/lib/Ui';
 
 @Component({
-    selector: "app-device",
-    templateUrl: "./device.component.html",
+    selector: 'app-device',
+    templateUrl: './device.component.html',
 })
 export class DeviceComponent implements OnInit {
     text: string;
@@ -21,26 +22,39 @@ export class DeviceComponent implements OnInit {
         private bcs: ByzCoinService,
         private dialog: MatDialog,
     ) {
-        this.text = "Please wait";
+        this.text = 'Please wait';
     }
 
     async ngOnInit() {
         try {
             if (await this.bcs.hasUser()) {
-                if (!(await showDialogOKC(this.dialog, "Overwrite user?", "There seems to" +
-                    "be a user already defined on this browser. Do you want to overwrite it?",
-                    {OKButton: "Overwrite", CancelButton: "Keep existing"}))) {
-                    return this.router.navigate(["/"]);
+                await this.bcs.loadUser();
+                const url = new URL(window.location.href, true);
+                if (url.query.credentialIID) {
+                    const credID = Buffer.from(url.query.credentialIID, 'hex');
+                    if (credID.equals(this.bcs.user.credStructBS.id)) {
+                        await showDialogInfo(this.dialog, 'Already signed up',
+                            'You used the signup link to return back to the omniledger ' +
+                            'service. Please don\'t use the "Sign up" link twice. ' +
+                            'If you deleted your previous account, please follow the section ' +
+                            '"Troubleshooting" in the same account.', 'Understood');
+                        return this.router.navigateByUrl('/');
+                    }
+                }
+                if (!(await showDialogOKC(this.dialog, 'Overwrite user?', 'There seems to' +
+                    'be a user already defined on this browser. Do you want to overwrite it?',
+                    {OKButton: 'Overwrite', CancelButton: 'Keep existing'}))) {
+                    return this.router.navigateByUrl('/');
                 }
             }
-            await showTransactions(this.dialog, "Attaching to existing user",
+            await showTransactions(this.dialog, 'Attaching to existing user',
                 async (progress: TProgress) => {
-                    progress(50, "Attaching new device");
+                    progress(50, 'Attaching new device');
                     this.bcs.user = await this.bcs.retrieveUserByURL(window.location.href);
                 });
-            await this.router.navigate(["/"]);
+            await this.router.navigateByUrl('/');
         } catch (e) {
-            Log.catch(e, "Couldn't register:");
+            Log.catch(e, 'Couldn\'t register:');
             this.text = e.toString();
         }
     }
